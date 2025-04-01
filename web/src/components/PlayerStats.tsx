8089499d3d289e0c7848a3816d42615d67a18f32
CoffeeVictor <victor.jgfsilva@gmail.com>
2025-04-01 06:38:35 -0300

    Improves stat clarity

--- web/src/components/PlayerStats.tsx
+++ web/src/components/PlayerStats.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Player } from '../types';
+import { Color, Player } from '../types';
 interface Props {
 	players: Player[];
 	myId: string;
+}
+
+function getTextFromColor(color: Color): string {
+	const map: Record<Color, string> = {
+		blue: 'text-blue-500',
+		red: 'text-red-500',
+	};
+
+	return map[color];
 }
 
 export const PlayerStats: React.FC<Props> = ({ players, myId }) => {
 	return (
 		<div className='mt-4'>
 			{players.map((p) => (
-				<div key={p.id} className={`text-${p.color}-500`}>
-					{p.color}: <strong>{p.count}</strong>{' '}
-					{p.id === myId ? '← You' : ''}
+				<div key={p.id} className={`${getTextFromColor(p.color)}`}>
+					<span
+						className={`${p.id === myId && 'underline'} capitalize`}
+					>
+						{p.color}
+					</span>
+					: <strong>{p.count}</strong> {p.id === myId ? '← You' : ''}
 				</div>
 			))}
 		</div>
